83ad5a97b8b5cfa45767da9ad3fe55ae6c30ad7a
zephyronepointoh <97200193+zephyronepointoh@users.noreply.github.com>
2023-09-03 21:33:06 -0400

    Update chinese.py

--- DownUnderCTF 2023/chinese.py
+++ DownUnderCTF 2023/chinese.py
@@ -5,12 +5,8 @@ def chineseremainder(operators: list, results: list, printme: bool):
     for each in operators:
         opprod *= each
 
-    print(opprod)
-
     for eachh in operators:
         products.append(int(opprod/eachh))
-
-    print(products)
 
     for eachhh in products:
         n = 1
@@ -18,17 +14,16 @@ def chineseremainder(operators: list, results: list, printme: bool):
             n += 1
         multipliers.append(n)
 
-    print(multipliers)
-
     total = 0
     for i in range(len(operators)):
         total += (results[i] * products[i] * multipliers[i])
 
     base_original = int(total % opprod)
     if printme:
+        print(opprod)
+        print(products)
+        print(multipliers)
         print("With original values " + str(operators) + " and results " + str(results))
         print("The original number can be expressed by the formula " + str(base_original) + " + (" + str(opprod) + " * n) for any integer n.")
     return base_original, opprod
-
-
-print(chineseremainder([2, 3, 5, 7], [1, 1, 1, 2], False))
+    
